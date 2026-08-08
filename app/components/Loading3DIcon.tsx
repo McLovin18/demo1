99db12edfbdd840cdabbfcf0b5de0b4dacff2a85
McLovin18 <hwcobena@espol.edu.ec>
2026-08-08 15:11:19 -0500

    dine

--- app/components/Loading3DIcon.tsx
+++ app/components/Loading3DIcon.tsx
@@ -18,7 +18,7 @@ export function Loading3DIcon() {
 
       {/* Texto */}
       <p className="mt-5 text-sm text-[var(--textSecondary)] font-medium">
-        Cargando contenido de Art Design Makr
+        Cargando contenido de MVP Accesories
       </p>
 
     </div>
